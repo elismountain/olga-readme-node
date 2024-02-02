a7d4348e20da4388a6c8c0d728a1127f830a6369
elismountain <echospb@mail.ru>
2024-02-02 19:32:56 +0000

    created the readme markdown

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-// const fs = require("fs");
-// const path = require('path');
-// const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const util = require('util');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// // array of questions for user
-// const questions = [
 
-// ];
+// array of questions for user
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your application: ',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter the installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the usage information',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+      },
+      {
+        type: 'list',
+        name: 'licence',
+        message: 'What license would you like for your project?',
+        choices: ['MIT', 'None', 'GPLv2','LGPLv3', 'AFL-3.0']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input', 
+        name: 'email',
+        message: 'What is your email address?'
+      }
+    ]);
+  };
 
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
+  promptUser() 
+  .then((answers) => writeFileAsync('./README.md', generateMarkdown(answers)))
+  .then(() => console.log('Successfully wrote to readME'))
+  .catch((err) => console.error(err));
 
-// // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
 
